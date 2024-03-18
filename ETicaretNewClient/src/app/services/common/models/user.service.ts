ce9e1ee971cf97009e239063ce7ef95a9f93af7e
FatihAkpiyal <fatih.akpiyal1@gmail.com>
2024-03-18 20:28:25 +0300

    User Register Form and validators update || Connection with API

---
+++ ETicaretNewClient/src/app/services/common/models/user.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClientService } from '../http-client.service';
+import { User } from '../../../../Entities/user';
+import { Create_user } from '../../../contracts/users/create_users';
+import { Observable, firstValueFrom } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(private httpClientService: HttpClientService) { }
+
+
+  async create(user:User): Promise<Create_user>{
+    const observable: Observable<Create_user | User> = this.httpClientService.post<Create_user | User>({
+      controller:"users"
+
+    },user);
+
+    return await firstValueFrom(observable) as Create_user;
+  }
+
+}
--- ETicaretNewClient/src/app/ui/components/register/register.component.ts
+++ ETicaretNewClient/src/app/ui/components/register/register.component.ts
@@ -1,48 +1,81 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, ReactiveFormsModule, ValidationErrors, Validators } from '@angular/forms';
+import { User } from '../../../../Entities/user';
+import { UserService } from '../../../services/common/models/user.service';
+import { Create_user } from '../../../contracts/users/create_users';
+import { CustomToastrService, ToastrMessageType, ToastrPosition } from '../../../services/ui/custom-toastr.service';
+import { HttpClientService } from '../../../services/common/http-client.service';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [ReactiveFormsModule, CommonModule],
+  imports: [ReactiveFormsModule, CommonModule,HttpClientModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
+  styleUrl: './register.component.scss',
+  providers:[UserService,HttpClientService]
 })
 export class RegisterComponent {
-  
-  constructor(private formBuilder:FormBuilder) {}
 
-  frm:FormGroup;
+  constructor(private formBuilder: FormBuilder, private userService: UserService, private toastService: CustomToastrService) { }
 
-  ngOnInit(): void{
-    this.frm= this.formBuilder.group({
-      adSoyad: ["", [
-         Validators.required,
-         Validators.maxLength(50), 
-         Validators.minLength(3)]],
-      kullaniciAdi:["", [
+  frm: FormGroup;
+
+  ngOnInit(): void {
+    this.frm = this.formBuilder.group({
+      nameSurname: ["", [
         Validators.required,
-        Validators.maxLength(50), 
+        Validators.maxLength(50),
+        Validators.minLength(3)]],
+      userName: ["", [
+        Validators.required,
+        Validators.maxLength(50),
         Validators.minLength(3)]],
       email: ["", [
         Validators.required,
-        Validators.maxLength(250), 
+        Validators.maxLength(250),
         Validators.email]],
-      sifre:[""],
-      sifreTekrar:[""]
+      password: ["", [
+        Validators.required
+      ]],
+      passwordConfirm: ["", [
+        Validators.required
+      ]]
+    }, {
+      validators: (group: AbstractControl): ValidationErrors | null => {
+
+        let sifre = group.get("password").value;
+        let sifreTekrar = group.get("passwordConfirm").value;
+        return sifre === sifreTekrar ? null : { notSame: true };
+
+        return null;
+      }
     })
   }
 
-  get component(){
+  get component() {
     return this.frm.controls;
   }
 
-  submitted:boolean=false;
-  onSubmit(data: any){
-    this.submitted=true;
-    var c = this.component
-    debugger;
+  submitted: boolean = false;
+  async onSubmit(user: User) {
+    this.submitted = true;
+
+    if (this.frm.invalid)
+      return;
+
+    const result: Create_user = await this.userService.create(user);
+    if (result.succeeded)
+      this.toastService.message(result.message, "Kullanıcı Kaydı Başarılı", {
+        messageType: ToastrMessageType.Success,
+        position: ToastrPosition.TopRight
+      })
+    else
+      this.toastService.message(result.message, "Hata", {
+        messageType: ToastrMessageType.Error,
+        position: ToastrPosition.TopRight
+      })
   }
 
 }
