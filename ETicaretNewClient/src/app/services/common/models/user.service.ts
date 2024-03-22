c7192085d8da1111ccc34c8611d9a93483a5a645
FatihAkpiyal <fatih.akpiyal1@gmail.com>
2024-03-22 23:40:26 +0300

    Authorization updates for client perspective

--- ETicaretNewClient/src/app/services/common/models/user.service.ts
+++ ETicaretNewClient/src/app/services/common/models/user.service.ts
@@ -4,13 +4,15 @@ import { User } from '../../../../Entities/user';
 import { Create_user } from '../../../contracts/users/create_users';
 import { Observable, firstValueFrom } from 'rxjs';
 import { Token } from '../../../contracts/token/token';
+import { CustomToastrService, ToastrMessageType, ToastrPosition } from '../../ui/custom-toastr.service';
+import { TokenResponse } from '../../../contracts/token/tokenResponse';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor(private httpClientService: HttpClientService) { }
+  constructor(private httpClientService: HttpClientService, private toastrService : CustomToastrService) { }
 
 
   async create(user:User): Promise<Create_user>{
@@ -22,13 +24,23 @@ export class UserService {
     return await firstValueFrom(observable) as Create_user;
   }
 
-  async login(userNameOrEmail:string, password:string, callBackFunction?: ()=> void): Promise<any | Token>{
-   const observable:Observable<any | Token> = this.httpClientService.post<any | Token>({
+  async login(userNameOrEmail:string, password:string, callBackFunction?: ()=> void): Promise<any>{
+   const observable:Observable<any | TokenResponse> = this.httpClientService.post<any | TokenResponse>({
       controller:"users",
       action:"login"
     }, { userNameOrEmail, password })
 
-    const token: Token = await firstValueFrom(observable);
+    const tokenResponse: TokenResponse = await firstValueFrom(observable) as TokenResponse;
+
+    if(tokenResponse)
+      {
+        localStorage.setItem("accessToken",tokenResponse.token.accessToken);
+        
+        this.toastrService.message("Kullanıcı girişi başarıyla sağlanmıştır.", "Giriş Başarılı", {
+          messageType:ToastrMessageType.Success,
+          position:ToastrPosition.TopRight
+            })
+      }
     callBackFunction();
   }
 
